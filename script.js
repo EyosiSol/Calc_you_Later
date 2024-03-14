bee21d985a4bbd65fb2d89e4538789aff5fc7e36
eyosHP250 <eyosila123@gmail.com>
2024-03-14 21:35:39 +0300

    change the way the calculator works

--- script.js
+++ script.js
@@ -2,40 +2,58 @@ const clear = document.querySelector("#clear");
 const area = document.querySelector(".textArea");
 const del = document.querySelector("#Delete");
 const dot = document.querySelector("#dot");
-const buttons = document.querySelectorAll("button");
+const num0 = document.querySelector("#one");
+const num1 = document.querySelector("#two");
+const num2 = document.querySelector("#three");
+const num3 = document.querySelector("#four");
+const num4 = document.querySelector("#five");
+const num5 = document.querySelector("#six");
+const num6 = document.querySelector("#seven");
+const num7 = document.querySelector("#eight");
+const num8 = document.querySelector("#nine");
+const num9 = document.querySelector("#ten");
+const num00= document.querySelector("#twoZeros");
+const nums = document.querySelectorAll(".num")
+const add = document.querySelector("#add")
+const equals = document.querySelector("#equals")
+const sub = document.querySelector("#sub")
+const mult = document.querySelector("#mult")
+const div = document.querySelector("#divide")
+const percent = document.querySelector("#percentage")
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const value = button.textContent;
-    if (value === "C") {
-      area.textContent = "";
-      dot.disabled = false;
-    } else if( value === "="){
-        let statment = area.textContent;
-        if(statment.includes("x")){
-            let splitted = statment.split("x");
-            let num1 = splitted[0];
-            let num2 = splitted[1]
-            area.textContent = parseInt(num1) * parseInt(num2);
-        }       
-    }else if(value === "<="){
-        let content = area.textContent;
-        if(content.length > 0){
-            area.textContent = content.slice(0,-1);
-        }
-    }
-    else {
-      let areavalue = area.textContent;
-      if (areavalue.length < 6) {
-        if (value === ".") {
-          area.textContent += button.textContent;
-          dot.disabled = true;
-        } else {
-          area.textContent += button.textContent;
-        }
-      } else {
-        buttons.disabled = true;
-      }
-    }
-  });
+
+
+let startNew = true;
+let val1 = ''; 
+let val2 = ''
+clear.addEventListener("click",()=>{
+    area.textContent = "";
+    startNew = true;
+    val1 = '';
 });
+nums.forEach((numbers)=>{
+    numbers.addEventListener("click",()=>{
+    if(startNew === true){
+        area.textContent = '';
+        startNew = false;
+    }   
+    area.textContent += numbers.textContent;
+    })
+
+})
+
+add.addEventListener("click",()=>{
+    val1 += area.textContent;
+    startNew = true;
+})
+
+equals.addEventListener("click",()=>{
+    if(area.textContent === ''){
+        area.textContent = "ERR"
+    }
+    else{
+        val2 = area.textContent;
+        area.textContent = parseInt(val1) + parseInt(val2)
+    }
+   
+})
