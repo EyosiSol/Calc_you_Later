e21c9396d45fe342191895de44d5840833c80a38
eyosHP250 <eyosila123@gmail.com>
2024-03-14 15:51:46 +0300

    add functionality for multiplication button

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 const clear = document.querySelector("#clear");
 const area = document.querySelector(".textArea");
-const del = document.querySelector("#Delete")
-const dot = document.querySelector("#dot")
-const buttons = document.querySelectorAll("button")
-
+const del = document.querySelector("#Delete");
+const dot = document.querySelector("#dot");
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
-    button.addEventListener("click",()=>{
-        const value = button.value
-        if(value === 'clear'){
-           area.textContent = ""; 
-           dot.disabled = false;
-        }else{
-            let areavalue = area.textContent;
-            if(areavalue.length < 6){
-                if(value === "dot"){
-                    area.textContent += button.textContent;
-                    dot.disabled = true;
-                }else{
-                    area.textContent += button.textContent;
-                } 
-            }else{
-               buttons.disabled = true;
-            }
-            
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (value === "C") {
+      area.textContent = "";
+      dot.disabled = false;
+    } else if( value === "="){
+        let statment = area.textContent;
+        if(statment.includes("x")){
+            let splitted = statment.split("x");
+            let num1 = splitted[0];
+            let num2 = splitted[1]
+            area.textContent = parseInt(num1) * parseInt(num2);
+        }       
+    }
+    else {
+      let areavalue = area.textContent;
+      if (areavalue.length < 6) {
+        if (value === ".") {
+          area.textContent += button.textContent;
+          dot.disabled = true;
+        } else {
+          area.textContent += button.textContent;
         }
-    })
-      
-})
+      } else {
+        buttons.disabled = true;
+      }
+    }
+  });
+});
